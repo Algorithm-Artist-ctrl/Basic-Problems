cadb75c0d2cf55ea4f8fb9630d6dbe08b442cd72
Algorithm-Artist-ctrl <tarunkushwahkt@gmail.com>
2025-08-17 19:48:31 +0530

    User Define Swap_Two_Numbers

--- Swap_Two_Number.py
+++ Swap_Two_Number.py
@@ -1,6 +1,8 @@
+num1=int(input("Enter First Number:\n"))
+num2=int(input("Enter Second Number:\n"))
 print("\nBefore Swaping Numbers are :")
-a,b=2,3
+a,b=num1,num2
 print(a,b)
 print("\nAfter Swapping Numbers are")
-a,b=3,2
+a,b=num2,num1
 print(a,b)
