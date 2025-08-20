834236e2f8940790354b54dd057139e58d4fdb5b
TARUN KUSHWAH <TARUN@VIVEKs-MacBook-Air.local>
2025-08-20 19:10:29 +0530

    Name of day

---
+++ Day_of_Week.py
@@ -0,0 +1,16 @@
+usr_input=int(input("Input the Day Number:\n"))
+if usr_input==1:
+    print("Monday")
+elif usr_input==2:
+    print("Tuesday")
+elif usr_input==4:
+    print("Thursday")
+elif usr_input==5:
+    print("Friday")
+elif usr_input==6:
+    print("Saturday")
+elif usr_input==7:
+    print("Sunday")
+else:
+    print("Input Valid Day")
+    
