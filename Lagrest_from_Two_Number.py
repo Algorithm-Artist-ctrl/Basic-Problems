51902a420f570b5e193405a89a2518d2d7abda29
TARUN KUSHWAH <TARUN@VIVEKs-MacBook-Air.local>
2025-08-20 19:17:03 +0530

    check Largest Number

---
+++ Lagrest_from_Two_Number.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the First  numbers:\n"))
+num2=int(input("Enter the second number;\n"))
+if num1>num2:
+    print(f"Largest numbers is {num1}")
+else:
+    print(f"Largest numbers is {num2}")
