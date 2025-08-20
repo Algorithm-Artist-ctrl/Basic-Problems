0a272a64b3207bab110907455654ca5851f812a6
Algorithm-Artist-ctrl <tarunkushwahkt@gmail.com>
2025-08-20 20:50:49 +0530

    Update Day_of_Week.py

--- Day_of_Week.py
+++ Day_of_Week.py
@@ -1,16 +1,18 @@
 usr_input=int(input("Input the Day Number:\n"))
 if usr_input==1:
-    print("Monday")
-elif usr_input==2:
-    print("Tuesday")
-elif usr_input==4:
-    print("Thursday")
-elif usr_input==5:
-    print("Friday")
-elif usr_input==6:
-    print("Saturday")
-elif usr_input==7:
     print("Sunday")
+elif usr_input==2:
+    print("Monday")
+elif usr_input==3:
+    print("Tuesday")
+elif usr_input==5:
+    print("Wednesday")
+elif usr_input==6:
+    print("Thursday")
+elif usr_input==7:
+    print("Friday")
+elif usr_input==8:
+    print("Saturday")
 else:
     print("Input Valid Day")
     
