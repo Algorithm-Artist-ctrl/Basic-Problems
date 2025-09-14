808f43d96138d298d40f8b2560c158d3351502aa
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-09-14 23:35:52 +0530

    Calculate Cab charges

---
+++ Cab_Fare_Estimator.py
@@ -0,0 +1,11 @@
+def cab_Fare(km,time):
+    charge=float(input("Enter Per Km charge: "))
+    if time<=10:
+        return f"You have to pay",(charge*km)
+    else:
+        subcharge=float(input("Input Addition as you Drive in Night: " ))
+        amt=(charge*km)+subcharge
+        return f"You have to Pay {amt}"
+km=12
+time=7
+print(cab_Fare(12,7))
