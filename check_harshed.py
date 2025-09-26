a0522e9df54381994a19c6cd575beeaf8f18d4b2
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-09-26 22:29:45 +0530

    Fix

--- check_harshed.py
+++ check_harshed.py
@@ -1,11 +1,11 @@
 def hased(num):
-    sum=0
+    add=0
     temp=num
     while num>0:
         rem=num%10
-        sum=sum+rem
+        add=add+rem
         num=num//10
-    if temp%sum==0:
+    if temp%add==0:
         print("Harshad Number")
     else:
         print("Not a Harshad Number")
