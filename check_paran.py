442c9bd5ed6a8b3738c609a22cef42a6135976c8
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-09-29 22:44:43 +0530

    Check Paranthesis Validation

---
+++ check_paran.py
@@ -0,0 +1,17 @@
+def is_balanced(s):
+    stack = []
+    for char in s:
+        if char in "({[":
+            stack.append(char)
+        elif char in ")}]":
+            if not stack:
+                return False
+            top = stack.pop()
+            if (char == ')' and top != '(') or \
+               (char == ']' and top != '[') or \
+               (char == '}' and top != '{'):
+                return False
+    
+    return len(stack) == 0
+print(is_balanced("({[]})"))  
+print(is_balanced("([)]"))  
