42cec1304896d6719120c5a52ae8aa75cc673f7b
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-10-05 22:51:10 +0530

    Linear S ort

---
+++ LinearSorting.py
@@ -0,0 +1,14 @@
+def linear_sort(arr):
+    if not arr:
+        return []
+    max_val = max(arr)
+    count = [0] * (max_val + 1)
+    for num in arr:
+        count[num] += 1
+    sorted_arr = []
+    for i in range(len(count)):
+        sorted_arr.extend([i] * count[i])
+
+    return sorted_arr
+nums = [4, 2, 2, 8, 3, 3, 1]
+print(linear_sort(nums))
