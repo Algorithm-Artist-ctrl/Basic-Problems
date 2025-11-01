0bc6aba03754920bb299cf9885538f0ddc70f101
Tarun Kushwah <tarunkushwahkt@gmail.com>
2025-11-01 22:00:06 +0530

    Leetcode Problem

---
+++ palindrome2.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        return str(x) == str(x)[::-1]
+S=Solution()
+print(S.isPalindrome(121))
